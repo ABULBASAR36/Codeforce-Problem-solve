61bebc853d749f002e01084421a31380f3675b83
Abul_Basar <mdabulbasarbasar025@gmail.com>
2024-11-30 22:57:17 +0600

    update

--- 875_div2B.cpp
+++ 875_div2B.cpp
@@ -37,38 +37,121 @@ int main()
     }
 }*/
 
+// #include<bits/stdc++.h>
+// #define ll long long int
+// using namespace std;
+
+// int main() {
+//     ll t;
+//     cin >> t;
+//     while (t--) {
+//         ll n;
+//         cin >> n;
+//         ll a[n];
+//         ll b[n];
+//         for (int i = 0; i < n; i++) {
+//             cin >> a[i];
+//         }
+//         for (int i = 0; i < n; i++) {
+//             cin >> b[i];
+//         }
+//         map<int, int> fq;
+//         for (auto c : a) {
+//             fq[c]++;
+//         }
+//         for (auto c : b) {
+//             fq[c]++;
+//         }
+//         int mx = 0;
+//         for (auto it = fq.begin(); it != fq.end(); ++it) {
+//             mx = max(mx, it->second);
+//         }
+//         cout << mx << endl;
+//     }
+//     return 0;
+// }
+
+
 #include<bits/stdc++.h>
 #define ll long long int
 using namespace std;
+const int N = 1e5 + 5;
+const ll inf = 1e18;
 
-int main() {
-    ll t;
-    cin >> t;
-    while (t--) {
-        ll n;
-        cin >> n;
-        ll a[n];
-        ll b[n];
-        for (int i = 0; i < n; i++) {
-            cin >> a[i];
+int n, e;
+vector<ll> dis(N, inf);
+vector<ll> parent(N, -1);
+
+void dijkstra(ll source, vector<pair<ll, ll>> g[]) 
+{
+    set<pair<ll,ll>>st;
+    dis[source] = 0;
+    st.insert({0, source});
+
+    while (st.size()>0) 
+    {
+        auto x = *st.begin();
+        ll node= x.second;
+        ll cur_dist=x.first;
+        st.erase(st.begin());
+
+        if (cur_dist > dis[node]) 
+        continue; 
+
+        for (auto it : g[node]) 
+        {
+            ll to_node = it.second;
+            ll to_dist = it.first;
+
+            if (dis[to_node] > dis[node] + to_dist)
+             {
+                dis[to_node] = dis[node] + to_dist;
+                parent[to_node] = node; 
+                st.insert({dis[to_node], to_node});
+            }
         }
-        for (int i = 0; i < n; i++) {
-            cin >> b[i];
-        }
-        map<int, int> fq;
-        for (auto c : a) {
-            fq[c]++;
-        }
-        for (auto c : b) {
-            fq[c]++;
-        }
-        int mx = 0;
-        for (auto it = fq.begin(); it != fq.end(); ++it) {
-            mx = max(mx, it->second);
-        }
-        cout << mx << endl;
     }
-    return 0;
 }
 
+vector<ll> Backtrack(ll x) 
+{
+    vector<ll> path;
+    for (int i = x; i != -1; i= parent[i]) 
+    {
+        path.push_back(i);
+    }
+    reverse(path.begin(), path.end());
+    return path;
+}
 
+int main()
+{
+    cin >> n >> e;
+    vector<pair<ll, ll>> g[n + 1];
+
+    for (int i = 0; i < e; i++)
+    {
+        ll v1, v2, w;
+        cin >> v1 >> v2 >> w;
+        g[v1].push_back({w, v2});
+        g[v2].push_back({w, v1});
+    }
+
+    dijkstra(1, g);
+
+    if (dis[n] == inf) 
+    {
+        cout << -1 << endl; 
+    }
+    else 
+    {
+        vector<ll> path = Backtrack(n);
+        for (int node : path) 
+        {
+            cout << node << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
